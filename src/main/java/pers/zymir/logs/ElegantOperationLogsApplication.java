ec858d3a8366713dd1f9e79b3fbde5ce00dfef3a
zymir <amormz@foxmail.com>
2024-01-18 22:12:33 +0800

    feat: 初始化springboot项目

---
+++ src/main/java/pers/zymir/logs/ElegantOperationLogsApplication.java
@@ -0,0 +1,11 @@
+package pers.zymir.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegantOperationLogsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ElegantOperationLogsApplication.class, args);
+  }
+}
